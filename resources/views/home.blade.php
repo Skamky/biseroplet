957f7ad4ca84d36b16f6ccd29514d7da4d08a0af
Skamky <moskam@tuta.io>
2022-04-22 10:41:48 +0500

    добавление перевода

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('ru.Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,7 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                    {{__('auth.failed')}}
+                    {{__('30 Days')}}
                 </div>
             </div>
         </div>
