1bcf8731da74b1117bfb6474b41aa831794bcd0c
Skamky <moskam@tuta.io>
2022-04-26 14:07:28 +0500

    Фикс модальных окон

---
+++ app/Http/Controllers/ParserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\DomCrawler\Crawler;
+class ParserController extends Controller
+{
+    //
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,42 @@
     <div class="alert alert-info alert-dismissible " role="alert">
 Здесь будут представлены примеры схемы созданные в генераторе..
     </div>
-    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#Modal1">
+    <button type="button"  class="btn btn-secondary" onclick="showModal()">
         Click to launch modal
     </button>
 
-    <div class="modal fade" id="Modal1" tabindex="-1" role="dialog" aria-labelledby="Modal1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+
+    <div id="myModal" class="modal" tabindex="-1">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="ModalLabel">Here is a Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
+                    <h5 class="modal-title">Подтверждение</h5>
+                    <button type="button" class="close" data-dismiss="modal" onclick="closeModal()">×</button>
                 </div>
                 <div class="modal-body">
-                    Here goes the content of the modal.
+                    <p>Вы хотите сохранить изменения в этом документе перед закрытием?</p>
+                    <p class="text-secondary"><small>Если вы не сохраните, ваши изменения будут потеряны.</small></p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-warning">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="closeModal()">Закрыть</button>
+                    <button type="button" class="btn btn-primary">Сохранить изменения</button>
                 </div>
             </div>
         </div>
     </div>
-
+<script>
+    function showModal(){
+        $("#myModal").modal('show');
+    }
+    function closeModal()
+    {
+        $("#myModal").modal('hide');
+    }
+</script>
 @endsection
