c3db52e0dadd1939a87eda7fdbc890c0cf34037c
Skamky <moskam@tuta.io>
2022-04-28 01:12:06 +0500

    Очистка от мусора главной страницы

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,60 +5,6 @@
     </div>
 
 
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
 
-{{--    <div id="myModal" class="modal" tabindex="-1">--}}
-{{--        <div class="modal-dialog">--}}
-{{--            <div class="modal-content">--}}
-{{--                <div class="modal-header">--}}
-{{--                    <h5 class="modal-title">Подтверждение</h5>--}}
-{{--                    <button type="button" class="close" data-dismiss="modal" onclick="closeModal()">×</button>--}}
-{{--                </div>--}}
-{{--                <div class="modal-body">--}}
-{{--                    <p>Вы хотите сохранить изменения в этом документе перед закрытием?</p>--}}
-{{--                    <p class="text-secondary"><small>Если вы не сохраните, ваши изменения будут потеряны.</small></p>--}}
-{{--                </div>--}}
-{{--                <div class="modal-footer">--}}
-{{--                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="closeModal()">Закрыть</button>--}}
-{{--                    <button type="button" class="btn btn-primary">Сохранить изменения</button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--<script>--}}
-{{--    function showModal(){--}}
-{{--        $("#myModal").modal('show');--}}
-{{--    }--}}
-{{--    function closeModal()--}}
-{{--    {--}}
-{{--        $("#myModal").modal('hide');--}}
-{{--    }--}}
-{{--</script>--}}
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
 @endsection
