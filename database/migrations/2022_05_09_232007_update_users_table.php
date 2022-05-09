685e630d4796e320e71f89d02bb81c3a318670a8
Skamky <moskam@tuta.io>
2022-05-10 01:44:20 +0500

    создание миграции обновление таблицы пользователей

---
+++ database/migrations/2022_05_09_232007_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('status')
+                ->after('email')
+                ->default(1)
+                ->comment('Уровень доступа пользователя, где: 1- обычный пользователь; 10 модератор. ');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+
+    }
+}
