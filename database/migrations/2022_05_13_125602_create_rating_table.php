85feaf8880cab546c5e0991f07d65bddcf426a84
Skamky <moskam@tuta.io>
2022-05-18 19:43:23 +0500

    Фикс миграции рейтинга

--- database/migrations/2022_05_13_125602_create_rating_table.php
+++ database/migrations/2022_05_13_125602_create_rating_table.php
@@ -13,7 +13,7 @@ class CreateRatingTable extends Migration
      */
     public function up()
     {
-        Schema::create('rating', function (Blueprint $table) {
+        Schema::create('ratings', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_user');
             $table->unsignedBigInteger('id_scheme');
@@ -32,6 +32,6 @@ class CreateRatingTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rating');
+        Schema::dropIfExists('ratings');
     }
 }
